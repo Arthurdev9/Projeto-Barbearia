93391b91384cc13a98adfc84eb28d8ae3c53ba8c
Arthur Vinicius <arthurvinicius081@hotmail.com>
2026-01-27 10:34:09 -0300

    feat: ensure only authenticated user can book

--- src/app/_actions/create-booking.ts
+++ src/app/_actions/create-booking.ts
@@ -1,17 +1,24 @@
 'use server'
 
 import { prisma } from 'lib/prisma'
+import { getServerSession } from 'next-auth'
 import { revalidatePath } from 'next/cache'
+import { authOptions } from '../lib/auth'
 
 interface CreateBookingParams {
   serviceId: string
-  userId: string
   date: Date
 }
 
 export const createBooking = async (params: CreateBookingParams) => {
+  const user = await getServerSession(authOptions)
+
+  if (!user) {
+    throw new Error('Usuário não autenticado')
+  }
+
   await prisma.booking.create({
-    data: params
+    data: { ...params, userId: (user.user as any).id }
   })
   revalidatePath('/barbershops/[id]')
 }
---
+++ src/app/lib/auth.ts
@@ -0,0 +1,23 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { prisma } from 'lib/prisma'
+import { AuthOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+    })
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      session.user = {
+        ...session.user,
+        id: user.id
+      }
+      return session
+    }
+  }
+}
