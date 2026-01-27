ac775609949ce79e242a53e9327a5dd7dc16d939
Arthur Vinicius <arthurvinicius081@hotmail.com>
2026-01-27 10:19:14 -0300

    feat: ensure only signed in user can book

--- src/components/sidebar-sheet.tsx
+++ src/components/sidebar-sheet.tsx
@@ -7,20 +7,13 @@ import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from 'lucide-react'
 // import { Avatar, AvatarImage } from './ui/avatar'
 import Link from 'next/link'
 import Image from 'next/image'
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger
-} from './ui/dialog'
-import { signIn, signOut, useSession } from 'next-auth/react'
+import { Dialog, DialogContent, DialogTrigger } from './ui/dialog'
+import { signOut, useSession } from 'next-auth/react'
 import { Avatar, AvatarImage } from './ui/avatar'
+import SignInDialog from './sign-in-dialog'
 
 const Sidebar = () => {
   const { data } = useSession()
-  const handleLoginWithGoogleClick = () => signIn('google')
   const handleLogoutClick = () => signOut()
 
   console.log(data?.user?.image)
@@ -54,25 +47,7 @@ const Sidebar = () => {
                 </Button>
               </DialogTrigger>
               <DialogContent className="w-[90%]">
-                <DialogHeader>
-                  <DialogTitle>Faça login na plataforma</DialogTitle>
-                  <DialogDescription>
-                    Conecte-se usando sua conta do Google.
-                  </DialogDescription>
-                </DialogHeader>
-                <Button
-                  variant="outline"
-                  className="gap-1 font-bold"
-                  onClick={handleLoginWithGoogleClick}
-                >
-                  <Image
-                    src="/google.png"
-                    width={18}
-                    height={18}
-                    alt="Fazer login com o Google"
-                  />
-                  Google
-                </Button>
+                <SignInDialog />
               </DialogContent>
             </Dialog>
           </>
